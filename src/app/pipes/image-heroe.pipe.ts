355838a71c9d5ce587298d18a263d7a21a9dd049
Nico Corrales <ncorrales@conexia.com>
2024-12-22 18:31:47 -0300

    Se crea un pipe para poder acceder al contenedor de las imagenes y poder visualizarlas

---
+++ src/app/pipes/image-heroe.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Heroe } from '../interfaces/heroe.interface';
+
+@Pipe({
+  name: 'imageHeroe',
+  standalone: true
+})
+export class ImageHeroePipe implements PipeTransform {
+
+  transform( heroe: Heroe ): string {
+
+    if ( !heroe.id && !heroe.alt_img ) {
+      return './assets/no-image.png';
+    }
+
+    if ( heroe.alt_img ) return heroe.alt_img;
+
+    return `./assets/heroes/${ heroe.id }.jpg`;
+
+  }
+
+}
