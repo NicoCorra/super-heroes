9361cd9399dd84bfdbba2d3e60bf629be0c3b891
Nico Corrales <ncorrales@conexia.com>
2024-12-22 00:45:08 -0300

    Se crea la estructura del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './shared/pages/error404/error404.component';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { HeroesRoutingModule } from './heroes/heroes-routing.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => AuthRoutingModule,
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => HeroesRoutingModule,
+  },
+  {
+    path: '404',
+    component: Error404Component
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  }
+];
+
