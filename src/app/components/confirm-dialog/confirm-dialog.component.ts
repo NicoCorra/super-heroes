d6fe875a9635d89a8d7095cd7bb532b54fe9521c
Nico Corrales <ncorrales@conexia.com>
2024-12-26 12:52:04 -0300

    ajustes en confirmación dialog

--- src/app/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/confirm-dialog/confirm-dialog.component.ts
@@ -4,13 +4,11 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Heroe } from './../../interfaces/heroe.interface';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material/material.module';
-import { ImageHeroePipe } from '../../pipes/image-heroe.pipe';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-dialog',
   standalone: true,
-  imports: [CommonModule, MaterialModule, CommonModule, ImageHeroePipe, RouterLink],
+  imports: [CommonModule, MaterialModule, CommonModule],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.css'
 })
